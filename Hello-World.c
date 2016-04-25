ab5920e5a89be265a38531540ad7491faeb7b820
Sanjeev Baldia <baldia.sanjeev@gmail.com>
2016-04-25 18:18:44 +0530

    Create Hello-World.c

---
+++ Hello-World.c
@@ -0,0 +1,15 @@
+//My first program at GitHub
+
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+  int a,b,c;
+  cout<<"Enter Number 1:"
+  cin>>a;
+  cout<<"Enter Number 2:"
+  cin>>b;
+  c=a+b;
+  cout<<"Sum of two numbers "<<a<<" and "<<b<< " = "<<c;
+  getch();
+}
